d51ccd759ec6453e9b5ebad0197c36ab50f8bfd9
Héliton Nordt <hnordt@hnordt.com>
2016-04-06 06:50:49 -0300

    configureFetch

---
+++ src/utils/configureFetch.js
@@ -0,0 +1,24 @@
+import isPlainObject from 'lodash/isPlainObject'
+import merge from 'lodash/merge'
+
+export default function configureFetch(defaultOptions) {
+  return options => {
+    const {
+      url,
+      path,
+      body,
+      handleResponse = response => {
+        const { ok } = response
+        if (!ok) {
+          throw response
+        }
+        return response.json()
+      },
+      ...other
+    } = merge(defaultOptions, options)
+    return fetch(url + path, {
+      ...other,
+      body: isPlainObject(body) ? JSON.stringify(body) : body
+    }).then(handleResponse)
+  }
+}
