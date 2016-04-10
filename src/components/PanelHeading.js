fbb592270db5ccc4999186376d839b87d5812d42
Héliton Nordt <hnordt@hnordt.com>
2016-04-10 06:49:17 -0300

    Improve PanelHeading

--- src/components/PanelHeading.js
+++ src/components/PanelHeading.js
@@ -1,15 +1,16 @@
 import React, { PropTypes } from 'react'
+import PanelTitle from './PanelTitle'
 
 export default function PanelHeading(props) {
   const { title, children, ...other } = props
   return (
     <div {...other} className="panel-heading">
-      {title ? <h3 className="panel-title">{title}</h3> : children}
+      {title ? <PanelTitle>{title}</PanelTitle> : children}
     </div>
   )
 }
 
 PanelHeading.propTypes = {
-  title: PropTypes.node,
+  title: PropTypes.string,
   children: PropTypes.node
 }
