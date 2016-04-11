42322ca20e6a954e47e4b4544f24da350b5198fb
Héliton Nordt <hnordt@hnordt.com>
2016-04-11 01:50:52 -0300

    es6-promise

---
+++ src/components/Link.js
@@ -0,0 +1,18 @@
+import React, { PropTypes } from 'react'
+
+export default function Link(props) {
+  const { onClick, children, ...other } = props
+  return (
+    <a {...other} href="#" onClick={event => {
+      event.preventDefault()
+      onClick && onClick()
+    }}>
+      {children}
+    </a>
+  )
+}
+
+Link.propTypes = {
+  onClick: PropTypes.func,
+  children: PropTypes.node.isRequired
+}
