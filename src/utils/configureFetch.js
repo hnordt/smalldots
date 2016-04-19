f7fbeaf67a3d408e363e28aae57bb87f714b94d7
Héliton Nordt <hnordt@hnordt.com>
2016-04-18 21:17:48 -0300

    Remove Redux, update fetch

--- src/utils/configureFetch.js
+++ src/utils/configureFetch.js
@@ -1,13 +1,17 @@
 import isPlainObject from 'lodash/isPlainObject'
 
 export default function configureFetch({
-  requestEnhancer = request => request,
+  predicate = () => true,
+  requestTransformer = request => request,
   responseHandler = response => {
     return response.ok ? response.json() : Promise.reject(response)
   }
 }) {
   return request => {
-    const { url, body, ...other } = requestEnhancer(request)
+    const { url, body, ...other } = requestTransformer(request)
+    if (!predicate({ url, body, ...other })) {
+      return Promise.reject()
+    }
     return fetch(url, {
       ...other,
       body: isPlainObject(body) ? JSON.stringify(body) : body
