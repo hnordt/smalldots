f3ce9ef0369364712e2045bd6a229360875905af
Héliton Nordt <hnordt@hnordt.com>
2016-10-13 17:56:16 -0300

    Improve Fetch

--- src/Fetch.js
+++ src/Fetch.js
@@ -45,49 +45,40 @@ export default class Fetch extends Component {
   }
 
   fetch = (body = this.props.body) => {
-    return new Promise((resolve, reject) => {
-      this.setState({ fetching: true }, () => {
-        http.request({
-          method: this.props.method,
-          url: this.props.url,
-          params: this.props.params,
-          headers: this.props.headers,
-          data: body
-        }).then(response => {
-          if (this.willUnmount) {
-            return
+    this.setState({ fetching: true }, () => {
+      http.request({
+        method: this.props.method,
+        url: this.props.url,
+        params: this.props.params,
+        headers: this.props.headers,
+        data: body
+      }).then(response => {
+        if (this.willUnmount) {
+          return
+        }
+        this.setState({ fetching: false, response, data: response.data }, () => {
+          if (this.props.onResponse) {
+            this.props.onResponse(null, response)
           }
-          this.setState({ fetching: false, response, data: response.data }, () => {
-            if (this.props.onResponse) {
-              this.props.onResponse(response)
-            }
-            if (this.props.onData) {
-              this.props.onData(this.state.data)
-            }
-            resolve(this.state.data)
-          })
-        }).catch(error => {
-          if (this.willUnmount) {
-            return
+          if (this.props.onData) {
+            this.props.onData(this.state.data)
           }
-          if (!error.response) {
-            throw new Error(
-              `${error.message} on ${this.props.method.toUpperCase()} ${this.props.url}`
-            )
+        })
+      }).catch(error => {
+        if (this.willUnmount) {
+          return
+        }
+        this.setState({
+          fetching: false,
+          response: error.response || error,
+          error: error.response.data
+        }, () => {
+          if (this.props.onResponse) {
+            this.props.onResponse(error)
           }
-          this.setState({
-            fetching: false,
-            response: error.response,
-            error: error.response.data
-          }, () => {
-            if (this.props.onResponse) {
-              this.props.onResponse(error.response)
-            }
-            if (this.props.onError) {
-              this.props.onError(this.state.error)
-            }
-            reject(this.state.error)
-          })
+          if (this.props.onError) {
+            this.props.onError(this.state.error)
+          }
         })
       })
     })
