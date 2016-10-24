1f49a4ce749ade8e13df80b1fb5f634815b36933
Héliton Nordt <hnordt@hnordt.com>
2016-10-24 02:26:32 -0200

    Fix Link

--- src/Link.js
+++ src/Link.js
@@ -4,7 +4,6 @@ export default function Link({ onClick, ...rest }) {
   return (
     <a
       {...rest}
-      href="#"
       onClick={event => {
         event.preventDefault()
         if (onClick) {
