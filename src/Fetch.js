82c4cffd8a7b224f95256978a83cb4996283398f
Héliton Nordt <hnordt@hnordt.com>
2016-12-02 17:31:50 -0200

    #29

--- src/Fetch.js
+++ src/Fetch.js
@@ -1,14 +1,17 @@
-import { Component, PropTypes } from 'react'
+import { PureComponent, PropTypes } from 'react'
 import axios from 'axios'
 import isEqual from 'lodash/isEqual'
 
-const http = axios.create()
+let http = null
+if (typeof document !== 'undefined') {
+  http = axios.create()
+}
 
-export default class Fetch extends Component {
+class Fetch extends PureComponent {
   static propTypes = {
     method: PropTypes.oneOf(['get', 'post', 'put', 'delete']),
     url: PropTypes.string.isRequired,
-    params: PropTypes.object,
+    urlParams: PropTypes.object,
     headers: PropTypes.object,
     body: PropTypes.object,
     lazy: PropTypes.bool,
@@ -28,63 +31,63 @@ export default class Fetch extends Component {
   }
 
   componentDidMount() {
-    if (!this.props.lazy) {
-      this.fetch()
+    if (this.props.lazy) {
+      return
     }
+    this.fetch()
   }
 
   componentWillReceiveProps(nextProps) {
-    if (!this.props.lazy && !isEqual(this.props, nextProps)) {
-      this.fetch()
+    if (this.props.lazy || isEqual(this.props, nextProps)) {
+      return
     }
+    this.fetch()
   }
 
   componentWillUnmount() {
     this.willUnmount = true
   }
 
-  fetch = (body = this.props.body) => {
-    this.setState({ fetching: true }, () => {
-      http.request({
-        method: this.props.method,
-        url: this.props.url,
-        params: this.props.params,
-        headers: this.props.headers,
-        data: body
-      }).then(response => {
-        if (this.willUnmount) {
-          return
-        }
-        this.setState({
-          fetching: false,
-          response,
-          data: response.data,
-          error: null
-        }, () => {
-          if (this.props.onResponse) {
-            this.props.onResponse(null, this.state.response)
-          }
-          if (this.props.onData) {
-            this.props.onData(this.state.data)
-          }
-        })
-      }).catch(error => {
-        if (this.willUnmount) {
-          return
-        }
-        this.setState({
-          fetching: false,
-          response: error,
-          error
-        }, () => {
-          if (this.props.onResponse) {
-            this.props.onResponse(this.state.response)
-          }
-          if (this.props.onError) {
-            this.props.onError(this.state.error)
-          }
-        })
+  fetch = props => {
+    this.setState({ fetching: true })
+    http.request({
+      method: this.props.method,
+      url: this.props.url,
+      params: this.props.urlParams,
+      headers: this.props.headers,
+      data: body,
+      ...props
+    }).then(response => {
+      if (this.willUnmount) {
+        return
+      }
+      this.setState({
+        fetching: false,
+        response,
+        data: response.data,
+        error: null
       })
+      if (this.props.onResponse) {
+        this.props.onResponse(null, response)
+      }
+      if (this.props.onData) {
+        this.props.onData(response.data)
+      }
+    }).catch(error => {
+      if (this.willUnmount) {
+        return
+      }
+      this.setState({
+        fetching: false,
+        response: error,
+        error
+      })
+      if (this.props.onResponse) {
+        this.props.onResponse(error)
+      }
+      if (this.props.onError) {
+        this.props.onError(error)
+      }
     })
   }
 
@@ -101,3 +104,5 @@ export default class Fetch extends Component {
     }) || null
   }
 }
+
+export default Fetch
