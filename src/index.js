e7d49345b826e2de901ad686efcbb45e5351f1a9
Héliton Nordt <hnordt@hnordt.com>
2016-12-08 18:48:14 -0200

    Improve importing

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import Fetch from './Fetch'
+import Form from './Form'
+import Link from './Link'
+import Router from './Router'
+import Storage from './Storage'
+import Toggler from './Toggler'
+
+export {
+  Fetch,
+  Form,
+  Link,
+  Router,
+  Storage,
+  Toggler
+}
