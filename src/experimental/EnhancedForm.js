2f1b1db91ce8c4c0d93a600d7b7203e3aa0be36b
Héliton Nordt <hnordt@hnordt.com>
2017-05-23 20:11:35 -0300

    Upgrade to React 15.5.x / Implement field.hidden in EnhancedForm

--- src/experimental/EnhancedForm.js
+++ src/experimental/EnhancedForm.js
@@ -1,37 +1,49 @@
-import React, { Component, PropTypes, cloneElement } from 'react'
-import uniq from 'lodash/uniq'
-import Form from '../Form'
-import Validator from '../Validator'
-import Navigator from '../Navigator'
+import React, { Component, cloneElement } from "react"
+import PropTypes from "prop-types"
+import uniq from "lodash/uniq"
+import Form from "../Form"
+import Validator from "../Validator"
+import Navigator from "../Navigator"
 
 export default class EnhancedForm extends Component {
   static propTypes = {
-    fields: PropTypes.arrayOf(PropTypes.shape({
-      tab: PropTypes.string,
-      label: PropTypes.node,
-      path: PropTypes.string.isRequired,
-      input: PropTypes.oneOfType([PropTypes.element, PropTypes.func]).isRequired,
-      size: PropTypes.number
-    })).isRequired,
+    fields: PropTypes.arrayOf(
+      PropTypes.shape({
+        tab: PropTypes.string,
+        label: PropTypes.node,
+        path: PropTypes.string.isRequired,
+        input: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+          .isRequired,
+        hidden: PropTypes.oneOfType([PropTypes.bool, PropTypes.func]),
+        size: PropTypes.number
+      })
+    ).isRequired,
     initialValues: PropTypes.object,
     validations: PropTypes.object,
     onSubmit: PropTypes.func,
     children: PropTypes.func.isRequired
   }
 
-  getForm() {
-    return this.form
-  }
+  getForm = () => this.form
+
+  getFields = () =>
+    this.props.fields.filter(
+      field =>
+        !(typeof field.hidden === "function"
+          ? field.hidden(this.form.values, this.parseForm(this.form))
+          : field.hidden)
+    )
 
   getTabs = () => {
-    if (!this.props.fields.find(field => field.tab)) {
-      return ['']
+    const fields = this.getFields()
+    if (!fields.find(field => field.tab)) {
+      return [""]
     }
-    return uniq(this.props.fields.filter(field => field.tab).map(field => field.tab))
+    return uniq(fields.filter(field => field.tab).map(field => field.tab))
   }
 
   getFieldsByTab = tab => {
-    return this.props.fields.filter(field => {
+    return this.getFields().filter(field => {
       return !field.tab || field.tab === tab
     })
   }
@@ -68,7 +80,7 @@ export default class EnhancedForm extends Component {
   submit = () => this.form.handleSubmit()
 
   handleSubmit = values => {
-    this.props.fields.forEach(field => this.form.setDirty(field.path))
+    this.getFields().forEach(field => this.form.setDirty(field.path))
     const errors = this.validator.getErrors()
     if (errors) {
       return
@@ -80,10 +92,12 @@ export default class EnhancedForm extends Component {
 
   renderInput = (form, field, errors) => {
     const value = form.getValue(field.path)
-    const setValue = event => form.setValue(field.path, (
-      event && event.target ? event.target.value : event
-    ))
-    if (typeof field.input === 'function') {
+    const setValue = event =>
+      form.setValue(
+        field.path,
+        event && event.target ? event.target.value : event
+      )
+    if (typeof field.input === "function") {
       return field.input({
         form: this.parseForm(form),
         field,
@@ -99,38 +113,50 @@ export default class EnhancedForm extends Component {
     const tabs = this.getTabs()
     return (
       <Form
-        ref={form => this.form = form}
+        ref={form => (this.form = form)}
         initialValues={this.props.initialValues}
         noValidate={true}
         onSubmit={this.handleSubmit}
       >
         {form => (
           <Validator
-            ref={validator => this.validator = validator}
+            ref={validator => (this.validator = validator)}
             validations={this.props.validations}
             values={form.values}
           >
             {validator => (
               <Navigator initialScene={tabs[0]}>
-                {navigator => tabs.reduce((result, tab) => ({
-                  ...result,
-                  [tab]: this.props.children({
-                    ...this.parseForm(form),
-                    tabs: tabs[0] !== '' && tabs.map(tab => ({
-                      label: tab,
-                      active: tab === navigator.currentScene,
-                      errors: this.getErrorsByTab(validator.errors, tab),
-                      onClick: () => navigator.setScene(tab)
-                    })),
-                    fields: this.getFieldsByTab(navigator.currentScene).map(field => ({
-                      ...field,
-                      input: this.renderInput(form, field, validator.errors),
-                      error: validator.errors && validator.errors[field.path]
-                    })),
-                    values: form.values,
-                    errors: validator.errors
-                  })
-                }), {})}
+                {navigator =>
+                  tabs.reduce(
+                    (result, tab) => ({
+                      ...result,
+                      [tab]: this.props.children({
+                        ...this.parseForm(form),
+                        tabs: tabs[0] !== "" &&
+                          tabs.map(tab => ({
+                            label: tab,
+                            active: tab === navigator.currentScene,
+                            errors: this.getErrorsByTab(validator.errors, tab),
+                            onClick: () => navigator.setScene(tab)
+                          })),
+                        fields: this.getFieldsByTab(
+                          navigator.currentScene
+                        ).map(field => ({
+                          ...field,
+                          input: this.renderInput(
+                            form,
+                            field,
+                            validator.errors
+                          ),
+                          error: validator.errors &&
+                            validator.errors[field.path]
+                        })),
+                        values: form.values,
+                        errors: validator.errors
+                      })
+                    }),
+                    {}
+                  )}
               </Navigator>
             )}
           </Validator>
