cfc9397a6495645c8875f520e3a7dab82b273126
Héliton Nordt <hnordt@hnordt.com>
2018-09-21 20:46:23 -0300

    Export utils

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ export { default as Compose } from "./Compose"
 export { default as Component } from "./Component"
 
 export { default as createStore } from "./createStore"
+
+export { default as identity } from "./utils/identity"
+export { default as isPlainObject } from "./utils/isPlainObject"
+export { default as isFn } from "./utils/isFn"
+export { default as mapValues } from "./utils/mapValues"
+export { default as filterValues } from "./utils/filterValues"
+export { default as compose } from "./utils/compose"
