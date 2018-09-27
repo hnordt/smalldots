6d6e2bf6b3851ac60e56f59cd468fe93fd94613b
Héliton Nordt <hnordt@hnordt.com>
2018-09-27 16:22:34 -0300

    Add callAl

--- src/createError.js
+++
@@ -1,9 +0,0 @@
-let id = 0
-
-const createError = message => {
-  const error = new Error(message)
-  error.id = id++
-  return Promise.resolve(error)
-}
-
-export default createError
--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ export { default as Compose } from "./Compose"
 
 export { default as Component } from "./Component"
 
-export { default as createError } from "./createError"
 export { default as createStore } from "./createStore"
 
 export { default as identity } from "./utils/identity"
@@ -10,4 +9,5 @@ export { default as isPlainObject } from "./utils/isPlainObject"
 export { default as isFn } from "./utils/isFn"
 export { default as mapValues } from "./utils/mapValues"
 export { default as filterValues } from "./utils/filterValues"
+export { default as callAll } from "./utils/callAll"
 export { default as compose } from "./utils/compose"
