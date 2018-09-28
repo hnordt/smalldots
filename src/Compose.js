fa76c044ab6ea1fe2301cb09b9263a9814c05da1
Héliton Nordt <hnordt@hnordt.com>
2018-09-28 17:00:42 -0300

    Improve Compose

--- src/Compose.js
+++ src/Compose.js
@@ -2,20 +2,22 @@ import { cloneElement } from "react"
 
 const render = (
   { components: [component, ...components], children },
-  acc = []
+  accumulator = []
 ) =>
   cloneElement(component, {
     children:
       typeof children === "function"
         ? (...args) =>
             components.length
-              ? render({ components, children }, [...acc, ...args])
-              : children(...acc, ...args)
+              ? render({ components, children }, [...accumulator, ...args])
+              : children(...accumulator, ...args)
         : components.length
           ? render({ components, children })
           : children
   })
 
 const Compose = ({ components, children }) => render({ components, children })
+
+Compose.defaultProps = { components: [] }
 
 export default Compose
